cebc7f25df2707d612d95f745478eb3a3d1f1933
Taylan Dogan <16501501+kondanta@users.noreply.github.com>
2018-03-05 18:09:50 +0300

    Init

---
+++ main.js
@@ -0,0 +1,7 @@
+const Parser = require("./src/parser");
+
+function run() {
+  const p = new Parser();
+  p.txtGenerator();
+}
+run()
---
+++ src/parser.js
@@ -0,0 +1,55 @@
+const request = require("request");
+const cheerio = require("cheerio");
+
+module.exports = class Parser {
+  constructor() {
+    this.url =
+        "https://www.hepsiburada.com/apple-iphone-6s-32-gb-apple-turkiye-garantili-p-HBV0000017G26"
+  }
+
+  /*
+   * Returns to the data from website
+   * String url Url of the page
+   * Function callback Callback function
+   */
+  get(url, callback) {
+    // Assuming net connection is fine
+    request(url,
+            function(error, response, body) { callback(null, error, body); });
+  }
+
+  printer(data) { console.log(data[0]["Marka"]); }
+
+  /*
+   * Parsing the website and create key value pairs for txt/json creation.
+   * Function callback Callback function that provides key/value pairs.
+   */
+  // TODO: DB integration
+  parser(callback) {
+    let data = [];
+    // productTechSpecContainer
+    this.get(this.url, (error, response, body) => {
+      // guard
+      if (error) {
+        consoe.log("error");
+        return -1;
+      }
+      const $ = cheerio.load(body);
+      $(body)
+          .find("#productTechSpecContainer table.data-list.tech-spec tbody tr")
+          .each(function(index, elem) {
+            let key = ($(elem).find("th").text());
+            let value = ($(elem).find("td").text());
+            if (value == "Yok") {
+              value = "N/A";
+            }
+            data.push({[key] : value})
+          });
+      setTimeout(function() { callback(null, data); }, 5000);
+    });
+  }
+
+  txtGenerator() {
+    this.parser((error, data) => { console.log(data); });
+  }
+};
