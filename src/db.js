10459c20e31c5c4477f1e6f20184ec89c9216eaa
Taylan Dogan <16501501+kondanta@users.noreply.github.com>
2018-03-10 14:52:34 +0300

    Add database interface

---
+++ src/db.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const productModel = require("./models/product_model");
+const categoryModel = require("./models/catalog_model");
+
+let Schema = mongoose.Schema;
+mongoose.connect("mongodb://kon:111111@ds261088.mlab.com:61088/zenk");
+
+module.exports = class Db {
+  constructor() { this.name = ""; }
+  getCatagory() {
+    var modal = categoryModel;
+    modal.find({}, function(err, data) { console.log(data); });
+  }
+
+  getPoduct() {
+    var product = productModel;
+    product.find({}, function(err, data) { console.log(data); });
+  }
+
+  insertIntoProduct(data) {
+    // getting connection
+    var db = mongoose.connection;
+    var Product = productModel;
+    var newProduct = new Product({
+      productName : "Test",
+      categoryID : 1,
+      attributes : data,
+      updateDate : new Date().getDate()
+    });
+    console.log(newProduct);
+  }
+
+  close() {
+    setTimeout(function() { mongoose.connection.close(); }, 2000);
+  }
+};
