324d1300c3f503f76125da1b867c9a2e993cd2c7
Taylan Dogan <16501501+kondanta@users.noreply.github.com>
2018-03-10 16:35:15 +0300

    Testing

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const Parser = require("./src/parser");
+const DB = require("./src/db");
 
 function run() {
-  const p = new Parser();
-  p.simpleSearch("Stok kodu");
-  p.parser((err, data) => { console.log(data); });
+  // how to use parser.
+  /* parser = new Parser(
+   *     "https://www.hepsiburada.com/apple-iphone-8-plus-64-gb-apple-turkiye-garantili-p-HBV000007PV9M")
+   * parser.parser((error, data) => {}); */
+  db = new DB();
+  db.getPoduct();
+  db.close();
 }
+
 run()
